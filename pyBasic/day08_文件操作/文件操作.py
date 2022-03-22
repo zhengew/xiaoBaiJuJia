e1771f038a12b9673723118b34d18f7b86dd4b58
erwei.zheng <zhengew@foxmail.com>
2022-03-22 22:35:01 +0800

    文件的读写 dev

--- pyBasic/day08_文件操作/文件操作.py
+++ pyBasic/day08_文件操作/文件操作.py
@@ -1,4 +1,14 @@
-f = open(".\\test.txt", mode="r", encoding="utf-9")
-content = f.read()
-print(content)
-f.close()
+# f = open("test.txt", mode="r", encoding="utf-8")
+# content = f.read()
+# print(content)
+# f.close()
+
+with open("test.txt", mode="r", encoding="utf-8") as f:
+    # msg = f.readline()
+    # print(msg.strip("\n"))
+    #
+    # msgs = f.readlines()
+    # print(msgs)
+
+    for line in f:
+        print(line.strip())
