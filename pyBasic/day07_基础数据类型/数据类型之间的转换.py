e12914a5ddf117703d00024e5314d5435dfabbb1
erwei.zheng <zhengew@foxmail.com>
2022-03-22 21:35:21 +0800

    day07基本数据类型补充及编码 dev

--- pyBasic/day07_基础数据类型/数据类型之间的转换.py
+++ pyBasic/day07_基础数据类型/数据类型之间的转换.py
@@ -32,30 +32,43 @@ bool -> str  str(True) -> "True"
 ''' list set 之间的转换 '''
 
 # list -> set
-list1 = [1, 2, 3]
-set1 = set(list1)
-print(set1, type(set1))
+# list1 = [1, 2, 3]
+# set1 = set(list1)
+# print(set1, type(set1))
 
 # set - > list
-set2 = {1, 2, 3}
-list2 = list(set2)
-print(list2, type(list2))
+# set2 = {1, 2, 3}
+# list2 = list(set2)
+# print(list2, type(list2))
 
 ''' str bytes 之间的转换 '''
 # encode decode
 
 # str - > bytes
-str = "China中国"
-byte_str = str.encode(encoding="utf-8")
-print(byte_str) # b'China\xe4\xb8\xad\xe5\x9b\xbd'
+# str = "China中国"
+# byte_str = str.encode(encoding="utf-8")
+# print(byte_str) # b'China\xe4\xb8\xad\xe5\x9b\xbd'
 
 # bytes -> str
-str_byte = b'China\xe4\xb8\xad\xe5\x9b\xbd'
-str = str_byte.decode(encoding="utf-8")
-print(str)
+# str_byte = b'China\xe4\xb8\xad\xe5\x9b\xbd'
+# str = str_byte.decode(encoding="utf-8")
+# print(str)
 
 ''' 所有数据都可以转换为bool '''
-# "", 0, (), {}, set(), [], None - > 转化成的布尔值都是False
+
+''' 编码 '''
+# 不同编码集之间的转换，通过Unicode 编码和解码
+
+str = "你好，中国。"
+
+byte_str_utf8 = str.encode(encoding="utf-8")
+print(byte_str_utf8)
+unique_str = byte_str_utf8.decode(encoding="utf-8")
+byte_str_gbk = unique_str.encode(encoding="gbk")
+print(byte_str_gbk)
+print(byte_str_gbk.decode(encoding="gbk"))
+
+# 流程就是 先把 str -> utf-8编码bytes类型，在解码成 Unicode, 再用gbk类型编码bytes类型，最后再解码成gbk类型显示
 
 
 
