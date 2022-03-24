0a2d8af90b92cffda0093cf3d4b03f0359bfa318
erwei.zheng <zhengew@foxmail.com>
2022-03-24 11:17:52 +0800

    day11迭代器

--- pyBasic/day10_函数的作用域_内置函数_函数嵌套/homework/day10作业.py
+++ pyBasic/day10_函数的作用域_内置函数_函数嵌套/homework/day10作业.py
@@ -1,50 +1,45 @@
 # # 1.写函数，函数可以支持接收任意数字（位置传参）并将所有数据相加并返回。
-# def jia(a, b, c):
-#     if type(a) == int and type(b) == int and type(c) == int:
-#         return a+b+c
+# def add(*args):
+#     try:
+#         sum = 0.0
+#         for i in args:
+#             sum += i
+#         return sum
+#     except Exception:
+#         return "数据类型错误!"
 #
-# #
-# # 2.看代码写结果
-# #
-# #
-# # def func():
-# #     return 1, 2, 3
-# #
-# #
-# # val = func()
-# # print(type(val) == tuple)     True
-# # print(type(val) == list)      False
+# print(add(1, 2, 3, 5.5))
+
+# 2.看代码写结果
+# def func():
+#     return 1, 2, 3
 #
-#
-#
-#
+# val = func()
+# print(type(val) == tuple) # True
+# print(type(val) == list)  # False
+
 # # 3.看代码写结果
-# #
-# #
-# # def func(*args, **kwargs):
-# #     pass
-# #
-# #
-# # # a. 请将执行函数，并实现让args的值为 (1,2,3,4)
 # def func(*args, **kwargs):
-#     return  args
+#     pass
+# a. 请将执行函数，并实现让args的值为 (1,2,3,4)
+# def func(*args, **kwargs):
+#     return args
 #
 # func(1, 2, 3, 4)
 #
-# # # b. 请将执行函数，并实现让args的值为 ([1,2,3,4],[11,22,33])
+# # b. 请将执行函数，并实现让args的值为 ([1,2,3,4],[11,22,33])
 # def func(*args, **kwargs):
-#     return  args
+#     return args
 #
-# func([1, 2, 3, 4],[11, 22, 33])
-#
-#
-# # # c. 请将执行函数，并实现让args的值为 ([11,22],33) 且 kwargs的值为{'k1':'v1','k2':'v2'}
+# print(func([1, 2, 3, 4], [11, 22, 33]))
+
+# c. 请将执行函数，并实现让args的值为 ([11,22],33) 且 kwargs的值为{'k1':'v1','k2':'v2'}
 # def func(*args, **kwargs):
-#     return  args,kwargs
+#     return args,kwargs
 #
-# func([11, 22], 33, k1='v1', k2='v2')
-#
-# # # d. 如执行 func(*{'武沛齐','金鑫','女神'})，请问 args和kwargs的值分别是？
+# print(func([11, 22], 33, k1="v1", k2="v2"))
+
+# d. 如执行 func(*{'武沛齐','金鑫','女神'})，请问 args和kwargs的值分别是？
 # #args:('武沛齐','金鑫','女神')
 # #kwargs:{}
 #
@@ -279,21 +274,20 @@
 # '''字典:使用**dic'''
 # '''例:'''
 # def func(*args, **kwargs):
-#     return  args,kwargs
+#     return args, kwargs
 #
-# func(*[1, 2, 3, 4], **{'k1':'v1', 'k2':'v2'})
-#
-#
+# print(func(*[1, 2, 3], *[2, 3, 4], **{"a":1, "b":2}))
+
+
 # # 17.写函数, 接收两个数字参数, 将较小的数字返回.
-# #
-# def func_deci(a, b):
-#     if type(a) == int and type(b) == int:
+# def minNum(a, b):
+#     try:
 #         return a if a < b else b
+#     except TypeError:
+#         return "数据类型错误!"
 #
-#
-#
-#
-#
+# print(minNum(1, 2), minNum(3, 2), minNum(2.0, -1), minNum("zew", 1))
+
 # # 18.写函数, 接收一个参数(此参数类型必须是可迭代对象), 将可迭代对象的每个元素以’_’相连接,
 # # 形成新的字符串, 并返回.
 # #
