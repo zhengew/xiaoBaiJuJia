701a3ca1ed2192ac0d23f5b047756d85c792a1b5
erwei.zheng <zhengew@foxmail.com>
2022-03-28 20:24:33 +0800

    day18 博客园作业及软件开发规范

--- pyBasic/day17_自定义模块及内置模块复习/hashlib模块总结.py
+++ pyBasic/day17_自定义模块及内置模块复习/hashlib模块总结.py
@@ -18,3 +18,28 @@ m = hashlib.md5(r'test'.encode('utf-8')) # 加盐之后再加密，避免相同�
 m.update('123456'.encode('utf-8'))
 pwd = m.hexdigest()
 print(pwd)
+
+''' 加盐
+动态的盐
+'''
+m = hashlib.md5(r'test'[::2].encode('utf-8')) # 'test'[::2] 动态的盐，盐是不固定的
+pwd = m.hexdigest()
+print(pwd)
+
+''' sha系列   金融类，安全读比较高的行业用
+随着数字越高，加密越复杂，越不易破解，但耗时越长。
+'''
+m = hashlib.sha256(r'test'.encode('utf-8')) # 加盐之后再加密，避免相同密码被撞库破解
+m.update('123456'.encode('utf-8'))
+pwd = m.hexdigest()
+print(pwd)
+
+
+''' 文件的校验
+
+linux 中一切皆文件：文本文件，非文本文件，音频，视频，图片...
+无论你下载的视频,还是软件(国外的软件),往往都会有一个md5值
+'''
+
+# 练习题：
+# 通过分段加密下载文件，获取最终的加密字符串，校验官方提供的加密字符串是否相同，来判断文件是否被篡改过.
