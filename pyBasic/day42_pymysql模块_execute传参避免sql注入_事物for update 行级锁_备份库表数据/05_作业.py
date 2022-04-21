a5583a02a1ee4c3058f13ab37b0589dab5857f18
erwei.zheng <zhengew@foxmail.com>
2022-04-21 17:14:18 +0800

    day42 py基础 数据类型，函数，内置函数，内置模块，面向对象 网络编程 并发编程 mysql数据库 合理使用索引 索引命中 sql优化，已学完，准备下建立，下周面试

--- pyBasic/day42_pymysql模块_execute传参避免sql注入_事物for update 行级锁_备份库表数据/05_作业.py
+++ pyBasic/day42_pymysql模块_execute传参避免sql注入_事物for update 行级锁_备份库表数据/05_作业.py
@@ -1,3 +1,68 @@
 # 1. 写一个注册登陆 基于数据库来做
     # 加上 hashlib密文验证
 # 2. 另外10道题
+
+import pymysql
+import hashlib
+import sys
+
+def db():
+    try:
+        conn = pymysql.connect(user='root',
+                password='123456',
+                host='172.16.238.4',
+                database='day42',)
+
+        return conn
+    except Exception as e:
+        return e
+
+def get_md5(username, password):
+    m = hashlib.md5(username.encode('utf-8'))
+    m.update(password.encode('utf-8'))
+    return m.hexdigest()
+
+def register(username, password):
+    conn = db()
+    try:
+        cur = conn.cursor()
+        pwd = get_md5(username, password)
+        cur.execute("insert into teller values(null, %s, %s)", (username, pwd))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        conn.rollback()
+        return e
+
+def login(username, password):
+    conn = db()
+    try:
+        cur = conn.cursor()
+        ret = cur.execute("select username, password from teller where username = %s", (username,))
+        ret = cur.fetchone()
+        if ret:
+            uname, pwd = ret
+            password = get_md5(username,password)
+            if username == uname and password == pwd:
+                return True
+            else:
+                return False
+        else:
+            return False
+    except Exception as e:
+        return e
+
+def main():
+    opt_list = [('注册', 'register'), ('登陆', 'login')]
+    for index, opt in enumerate(opt_list, 1):
+        print(index, opt[0])
+    n = int(input('请选择:').strip())
+    if hasattr(sys.modules[__name__], opt_list[n-1][1]):
+        name = input('用户名:').strip()
+        pwd = input('密码:').strip()
+        ret = getattr(sys.modules[__name__], opt_list[n-1][1])(name, pwd)
+        print(ret)
+
+if __name__ == '__main__':
+    main()
