626cd3afbc4a53736b2027994bedc7060fc7d7d1
erwei.zheng <zhengew@foxmail.com>
2022-05-03 09:37:59 +0800

    redis 基本数据类型及常用方法

--- redis_非关系型/day01/redis初识.py
+++ redis_非关系型/day01/redis初识.py
@@ -79,6 +79,117 @@ nosql not only sql
     # type
         # 查看key所存储的数据类型，如果没有这个key，则返回none
 
+    # String
+        # set
+            # 设置key value
+            # 如果key存在则覆盖，不存在则新建
+            # EX second 设置key的存活时间，单位是秒
+            # PX 毫秒 设置key的存活时间，单位是毫秒
+            # NX 如果键不存在，则新建，如果不存在，则返回nil
+            # XX 只有键存在，才能操作
+        # get
+            # 获取key对应的value,只能获取一个，如果key存在，则返回值，如果key不存在，则返回nil
+        # mset
+            # 批量创建 key value 对应关系,会覆盖已存在的key
+        # mget
+            # 批量获取key,如果不存在，则返回nil
+        # getset
+            # 给指定的key设置新value,并返回原来的value,如果key不存爱，则返回nil
+        # strlen
+            # 返回value的长度
+        # append
+            # 如果key存在，则追加，如果key不存在，则新建
+        # incr
+            # 将key中存在的数加1，只能对数字有效
+        # decr
+            # 将key中存在的数减1，只能对数字有效
+        # incrby
+            # 将key中存在的数值，指定增加多少，只对数字有效
+        # decrby
+            # 将key中存在的数值，减少指定的值，只能对数字有效
+        # getrange
+            # 切片，同python类似，不能使用步长
+        # incrbyfloat
+            # 将key中存在的数值，增加指定的浮点数
+        # lrem
+            # 删除列表中的value
+            # count > 0 从表头往表尾的方向查找，删除指定的个数
+            # count = 0, 删除全部
+            # count < 0, 从表尾的位置往表头的方向查找，删除指定的个数
+        # let
+            # 替换指定索引位置的value，如果索引超出范围，则报错
+        # ltrim
+            # 列表的切片
+
+    # hash
+        # {'db':{'redis':redis.conf, 'mysql':'mysql.cnf', 'nginx':'nginx.conf'}}
+        # hset
+            # 给hash增加 key-value 值
+        # hlen
+            # 获取hash的长度
+        # hget
+            # 获取某个hash里面key的value
+        # hgetall
+            # 获取所有的键值对
+        # hmset
+            # 批量增加键值对
+        # hmget
+            # 批量获取键值对
+        # hsetnx
+            # 给指定的hash增加键值对，如果原来的field存在，则操作无效,如果不存在，则新增
+        # hkeys
+            # 获取hash表中所有的field
+        # hvals
+            # 获取hash表中所有的value
+        # hdel
+            # 删除hash表中的一个或者多个 key-value
+        # hexists
+            # 判断hash表中的field是否存在，如果存在则为1，不存在则为0
+        # hincrby
+            # 给hash表中的key 增加指定的数值，只限于数字
+        # hincrbyfloat
+            # 给hash表中的key 增加指定的浮点数，只限于数字
+
+    # set
+        # sadd
+            # 给集合添加值，如果值存在，则什么都不操作，如果值不存在，则添加
+        # smembers
+            # 获取集合所有的成员
+        # scard
+            # 获取集合的个数
+        # sdiff
+            # 获取两个集合的差集,前面存在后面不存在的值
+        # sinter
+            # 获取两个集合的交集
+        # sunion
+            # 获取两个集合的并集
+        # sismember
+            # 判断元素是否在集合中，如果存在，则为1，如果不存在，则为0
+        # smove
+            # 将指定的元素从一个集合移动到另一个集合中，
+            # 如果原集合存在，则移动，如果不存则，则忽略
+            # 如果目标集合存在，则直接移动，如果目标集合不存在，则新建集合并移动
+        # spop
+            # 随机删除指定个数的元素，并把删除的元素打印出来
+        # srandmember
+            # 随机获取指定个数的元素
+            # 默认随机获取1个
+            # 如果conunt > 0
+                # 如果count > 集合元素个数，则全部取出
+                # 如果count < 集合元素个数，则随机取出count个元素
+            # 如果count < 0
+                # 则随机count的绝对值次取出值
+        # srem
+            # 删除指定的1个或者多个元素
+
+    # zset 有序集合
+        # zadd key [NX|XX] [CH] [INCR] score member [score member ...]
+        # 用法同 set
+
+
+
+
+
 
 
 
